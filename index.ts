1f83e4b7dfa7836f655da2f586722e087eafd511
Hani J <hanijandali@gmail.com>
2022-07-12 14:00:35 -0700

    Completion of functions in typescript, compiled code from typescript to javascript

--- index.ts
+++ index.ts
@@ -1,22 +1,21 @@
 
-const ordinals: string[] = ["th", "st", "nd", "rd"];
-
 function ordinalSuffix(n: number): string{
     // Make sure number is positive
     n = Math.abs(n);
     // Exception rule of 'teen' numbers ending in th
     const teenth_number: number = n % 100;
     if (teenth_number <= 20 && teenth_number >= 10){
-        return 'th'
+        return n + 'th'
     }
     const indicator_number: number = n % 10;
     if (indicator_number === 1){
-        return 'st'
+        return n + 'st'
     } else if (indicator_number === 2){
-        return 'nd'
+        return n + 'nd'
     } else if (indicator_number === 3){
-        return 'rd'
+        return n + 'rd'
     } else {
-        return 'th'
+        return n + 'th'
     }
 }
+
